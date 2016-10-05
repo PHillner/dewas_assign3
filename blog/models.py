3d19bb2010279ec601d345da93dea9122192301e
PHillner <hillner.patrik@gmail.com>
2016-10-05 18:00:16 +0300

    Enhance pages looks and functionalities

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,9 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     text = models.TextField()
-
-    @classmethod
-    def getById(cls, id):
-        return cls.objects.get(id=id)
+    time = models.DateTimeField()
 
     @classmethod
     def exists(cls, id):
--- blog/views.py
+++ blog/views.py
@@ -9,25 +9,25 @@ from models import Blog
 
 def home(request):
     t = get_template(home.html)
-    html = t.render(Context({'added':0,'removed':0}))
+    html = t.render(Context({'blogs':Blog.objects.order_by("time"),'added':0,'removed':0}))
     return HttpResponse(html)
 
 def blog(request, id):
     if Blog.exists(id):
-        blog = Blog.getById(id)
+        blog = Blog.objects.get(id=id)
     else:
         return None
     t = get_template(blog.html)
-    html = t.render(Context({'id':blog.id,'name':blog.name,'date':blog.date}))
+    html = t.render(Context({'blog':Blog.objects.get(id=id)}))
     return HttpResponse(html)
 
 def edit(request, id):
     if Blog.exists(id):
-        blog = Blog.getById(id)
+        blog = Blog.objects.get(id=id)
     else:
         return None
     t = get_template(edit.html)
-    html = t.render(Context({'id':blog.id,'name':blog.name,'date':blog.date}))
+    html = t.render(Context({'blog':Blog.objects.get(id=id)}))
     return HttpResponse(html)
 
 def add(request):
@@ -37,9 +37,9 @@ def add(request):
 
 def delete(request, id):
     if Blog.exists(id):
-        blog = Blog.getById(id)
+        blog = Blog.objects.get(id=id)
     else:
         return None
     t = get_template(home.html)
-    html = t.render(Context({'added':0,'removed':1}))
+    html = t.render(Context({'blog':blog,'added':0,'removed':1}))
     return HttpResponse(html)
