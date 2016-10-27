4a48a56a23443d9fd69598cfab25e6070fdd9a4a
Patrik <hillner.patrik@gmail.com>
2016-10-27 19:17:07 +0300

    Add field to the blog model to lock the blog from being edited by multiple users

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Blog(models.Model):
     name = models.CharField(max_length=50)
     text = models.TextField()
     time = models.DateTimeField()
+    locked = models.BooleanField(default=0)
 
     @classmethod
     def exists(cls, id):
